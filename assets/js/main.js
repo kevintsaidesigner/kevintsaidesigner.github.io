06e4e5c8be2d481396b87ac4486bbc8fd6a67022
Kevintsai717 <59856420+Kevintsai717@users.noreply.github.com>
2020-07-16 16:47:48 +0800

    1607

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove',function(e){
+	var x = e.clientX;
+	var y = e.clientY;
+	cursor.style.left = x + "px";
+	cursor.style.top = y + "px";
+});
+
+$(".nav li").click(function(){
+  $(".hamburger-container").click();
+});
